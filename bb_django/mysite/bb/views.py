992df432a30f2c799989948f3fabbd3407a075b7
yemmitt <trautmann.tim@googlemail.com>
2017-01-11 22:46:32 +0100

    IMPROVE: Implemented ranks and chose of the chose cake baker

--- bb_django/mysite/bb/views.py
+++ bb_django/mysite/bb/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
@@ -31,21 +33,25 @@ class Statistics:
                                           ).sum().groupby(level='name').count().sort_values(by='result', ascending=0)
         plays_df.columns = ['rel_plays', 'plays']
         plays = plays_df.loc[:, 'plays']
-        self.statisticsdict['plays'] = [x for x in zip(plays.index, plays)]
+        plays_ranks = plays.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['plays'] = [x for x in zip(plays_ranks, plays.index, plays)]
         # relative plays
         plays_df.loc[:, 'rel_plays'] = plays_df.loc[:, 'plays']/plays_total
         rel_plays = plays_df.loc[:, 'rel_plays']
-        self.statisticsdict['relativePlays'] = [x for x in zip(rel_plays.index, rel_plays)]
+        rel_plays_ranks = rel_plays.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['relativePlays'] = [x for x in zip(rel_plays_ranks, rel_plays.index, rel_plays)]
         # losses
         losses_df = loser_df.T.groupby(by='name').count().sort_values(by='round', ascending=0)
         losses = losses_df.iloc[:, 1]
-        self.statisticsdict['losses'] = [x for x in zip(losses.index, losses)]
+        losses_rank = losses.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['losses'] = [x for x in zip( losses_rank, losses.index, losses)]
         # relative losses
         rel_losses_df = pd.concat([plays_df.plays, losses_df.iloc[:, 1]], axis=1).replace(to_replace=np.nan, value=0)
         rel_losses_unsorted = rel_losses_df.loc[:, 'round']/rel_losses_df.loc[:, 'plays']
         rel_losses_sorted = rel_losses_unsorted.sort_values(axis=0, ascending=False)
         rel_losses = rel_losses_sorted.round(2)
-        self.statisticsdict['relativeLosses'] = [x for x in zip(rel_losses.index, rel_losses)]
+        rel_los_ranks = rel_losses.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['relativeLosses'] = [x for x in zip(rel_los_ranks, rel_losses.index, rel_losses)]
         # longest pointstreak and losingstreak
         pointstreak_df = throw_df_plays.sort_values(by=['name', 'date'])
         pointstreak_dict = {}
@@ -60,47 +66,61 @@ class Statistics:
             pointstreak_dict[player] = longest_pointstreak
             zerostreak_dict[player] = longest_zerostreak
         pointstreak_sorted = pd.Series(pointstreak_dict).sort_values(axis=0, ascending=False)
+        pointstreak_ranks = pointstreak_sorted.rank(method='min', ascending=False).astype(int)
         zerostreak_sorted = pd.Series(zerostreak_dict).sort_values(axis=0, ascending=False)
-        self.statisticsdict['pointstreak'] = [x for x in zip(pointstreak_sorted.index, pointstreak_sorted)]
-        self.statisticsdict['zerostreak'] = [x for x in zip(zerostreak_sorted.index, zerostreak_sorted)]
+        zerostreak_ranks = zerostreak_sorted.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['pointstreak'] = [x for x in zip(pointstreak_ranks,
+                                                             pointstreak_sorted.index, pointstreak_sorted)]
+        self.statisticsdict['zerostreak'] = [x for x in zip(zerostreak_ranks,
+                                                            zerostreak_sorted.index, zerostreak_sorted)]
         # gladiator
         gladiator_points_df = gladiator_df.T.groupby(by='name').sum().sort_values(by='result', ascending=0)
         gladiator_points = gladiator_points_df.loc[:, 'result']
-        self.statisticsdict['gladiatorPoints'] = [x for x in zip(gladiator_points.index, gladiator_points)]
+        glad_ranks = gladiator_points.rank(method='min', ascending=False).astype(int)
+        self.statisticsdict['gladiatorPoints'] = [x for x in zip(glad_ranks,
+                                                                 gladiator_points.index, gladiator_points)]
         # fluktuationsmonster
         throw_df_plays.result = throw_df_plays.loc[:, 'result'].apply(int)
         stderivation = throw_df_plays.loc[:, ['name', 'result']].groupby(by='name').std()
         stderivation_sorted = stderivation.sort_values(by='result', ascending=0)
-        self.statisticsdict['stdev'] = [x for x in zip(stderivation_sorted.index,
+        std_ranks = [int(x) for x in stderivation_sorted.rank(method='min', ascending=False).values]
+        self.statisticsdict['stdev'] = [x for x in zip(std_ranks,
+                                                       stderivation_sorted.index,
                                                        [round(x, 2) for x in stderivation_sorted.values])]
         # durchschnittspunktzahl
         mean = throw_df_plays.loc[:, ['name', 'result']].groupby(by='name').mean()
         mean_sorted = mean.sort_values(by='result', ascending=0)
-        self.statisticsdict['mean'] = [x for x in zip(mean_sorted.index, [round(x, 2) for x in mean_sorted.values])]
+        mean_ranks = [int(x) for x in mean_sorted.rank(method='min', ascending=False).values]
+        self.statisticsdict['mean'] = [x for x in zip(mean_ranks,
+                                                      mean_sorted.index, [round(x, 2) for x in mean_sorted.values])]
         # angeber
         three_point_df = throw_df_plays.loc[:, ['name', 'result']].copy()
         three_point_ix = [i for i, x in enumerate(throw_df_plays.loc[:, 'result']) if x == 3]
         three_point_df_n = three_point_df.iloc[three_point_ix, :].groupby(by='name').count()
         three_point_df_sorted = three_point_df_n.sort_values(by='result', ascending=0)
         if len(three_point_df_n) > 0:
-            self.statisticsdict['threePoints'] = [x for x in zip(three_point_df_sorted.index,
+            three_point_ranks = [int(x) for x in three_point_df_sorted.rank(method='min',ascending=False).values]
+            self.statisticsdict['threePoints'] = [x for x in zip(three_point_ranks,
+                                                                 three_point_df_sorted.index,
                                                                  [int(x) for x in three_point_df_sorted.values])]
         # Nachsitzer
         second_round = throw_df_plays.loc[:, ['name', 'round']].copy()
         second_round_ix = [i for i, x in enumerate(throw_df_plays.loc[:, 'round']) if x == 2]
         second_round_df = second_round.iloc[second_round_ix, :].groupby(by='name').count()
         second_round_sorted = second_round_df.sort_values(by='round', ascending=0)
-        self.statisticsdict['secondRound'] = [x for x in zip(second_round_sorted.index,
+        ranks = [int(x) for x in second_round_sorted.rank(method='min', ascending=False).values]
+        self.statisticsdict['secondRound'] = [x for x in zip(ranks,
+                                                             second_round_sorted.index,
                                                              [int(x) for x in second_round_sorted.values])]
 
         # Norm guy by Mathias Seibert
         norm_guy = []
-        norm_names = pd.DataFrame(index=pd.DataFrame(self.statisticsdict['mean']).iloc[:, 0])
+        norm_names = pd.DataFrame(index=pd.DataFrame(self.statisticsdict['mean']).iloc[:, 1])
         stat_keys = self.statisticsdict.keys()
         stat_keys.remove('TotalPlays')
         for key in stat_keys:
             temp = pd.DataFrame(self.statisticsdict[key])
-            temp.columns = ['name', 'result']
+            temp.columns = ['rank', 'name', 'result']
             temp.set_index('name', inplace=True)
             temp = norm_names.join(temp)
             temp.replace(np.nan, 0, inplace=True)
@@ -109,8 +129,36 @@ class Statistics:
             norm_guy.append(score_norm)
         norm_guy_result_df = pd.concat(norm_guy, axis=1).mean(axis=1).sort_values()
         norm_guy_result_df = 1 - norm_guy_result_df
-        self.statisticsdict['norm_guy'] = [x for x in zip(norm_guy_result_df.index,
+        norm_guy_ranks = [int(x) for x in norm_guy_result_df.rank(method='min', ascending=False).values]
+        self.statisticsdict['norm_guy'] = [x for x in zip(norm_guy_ranks,
+                                                          norm_guy_result_df.index,
                                                           [round(x, 2) for x in norm_guy_result_df.values])]
+        # Get Cake Guy
+        relevant_categories = self.statisticsdict.keys()
+        relevant_categories.remove('TotalPlays')
+        relevant_categories.remove('relativePlays')
+        winner_categorie = relevant_categories[np.random.random_integers(0, len(relevant_categories)-1)]
+        categorie_dict = {
+            'plays': u'Basketballjunkie',
+            'losses': u'Kaffegott',
+            'relativeLosses': u'Angestellter des Wasserkochers',
+            'pointstreak': u'Goldenes Händchen',
+            'zerostreak': u'Tennisarm',
+            'stdev': u'Fluktuationsmonster',
+            'mean': u'Solider Typ',
+            'gladiatorPoints': u'Gladiator',
+            'threePoints': u'Angeber',
+            'secondRound': u'Nachsitzer',
+            'norm_guy': u'Regular every day normal guy'
+        }
+        temp_df = pd.DataFrame(self.statisticsdict[winner_categorie])
+        temp_df.columns = ['rank', 'name', 'result']
+        rank1 = sum([x for x in temp_df['rank'] if x == 1])
+        if rank1 > 1:
+            cake_baker = temp_df.name[np.random.randint(0, rank1 - 1)]
+        else:
+            cake_baker = temp_df.name[0]
+        self.statisticsdict['mrcake'] = (categorie_dict[winner_categorie], cake_baker)
 
 
 def index(request):
